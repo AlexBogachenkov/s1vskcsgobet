abb23de9d25873bb16ed7b52c436324b154f4050
AlexBogachenkov <aleksbogac@gmail.com>
2021-03-13 21:40:01 +0200

    AddBetRequest @Builder added

--- src/main/java/s1vskcsgobet/core/requests/bet/AddBetRequest.java
+++ src/main/java/s1vskcsgobet/core/requests/bet/AddBetRequest.java
@@ -1,10 +1,12 @@
 package s1vskcsgobet.core.requests.bet;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 public class AddBetRequest {
 
     private String teamAName;
