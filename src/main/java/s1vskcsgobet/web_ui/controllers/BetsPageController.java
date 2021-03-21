48b544a24d94e707600f45998562f4422599f1c9
AlexBogachenkov <aleksbogac@gmail.com>
2021-03-21 15:23:49 +0200

    JS code replaced with Thymeleaf

--- src/main/java/s1vskcsgobet/web_ui/controllers/BetsPageController.java
+++ src/main/java/s1vskcsgobet/web_ui/controllers/BetsPageController.java
@@ -1,13 +1,22 @@
 package s1vskcsgobet.web_ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import s1vskcsgobet.core.responses.bet.FindAllBetsResponse;
+import s1vskcsgobet.core.services.BetService;
 
 @Controller
 public class BetsPageController {
 
+    @Autowired
+    private BetService betService;
+
     @GetMapping(value = "/bets")
-    public String showBetsPage() {
+    public String showBetsPage(ModelMap modelMap) {
+        FindAllBetsResponse response = betService.findActive();
+        modelMap.addAttribute("activeBets", response.getAllBets());
         return "bets";
     }
 
