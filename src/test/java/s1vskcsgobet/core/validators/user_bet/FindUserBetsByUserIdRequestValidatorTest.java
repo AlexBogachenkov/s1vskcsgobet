9e9d24a0b5717c679ebb879cfb63bcb19d330d66
AlexBogachenkov <aleksbogac@gmail.com>
2021-03-22 20:56:12 +0200

    FindUserBetsByUserIdRequestValidator tests added

---
+++ src/test/java/s1vskcsgobet/core/validators/user_bet/FindUserBetsByUserIdRequestValidatorTest.java
@@ -0,0 +1,55 @@
+package s1vskcsgobet.core.validators.user_bet;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import s1vskcsgobet.core.database.UserRepository;
+import s1vskcsgobet.core.requests.user_bet.FindUserBetsByUserIdRequest;
+import s1vskcsgobet.core.responses.CoreError;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class FindUserBetsByUserIdRequestValidatorTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private FindUserBetsByUserIdRequestValidator validator;
+
+    @Test
+    public void shouldReturnErrorList_whenIdIsNull() {
+        FindUserBetsByUserIdRequest request = new FindUserBetsByUserIdRequest(null);
+        List<CoreError> errors = validator.validate(request);
+
+        assertEquals(1, errors.size());
+        assertEquals("User ID", errors.get(0).getField());
+        assertEquals("must not be empty!", errors.get(0).getMessage());
+    }
+
+    @Test
+    public void shouldReturnErrorList_whenIdNotFound() {
+        FindUserBetsByUserIdRequest request = new FindUserBetsByUserIdRequest(2L);
+        Mockito.when(userRepository.existsById(2L)).thenReturn(false);
+        List<CoreError> errors = validator.validate(request);
+
+        assertEquals(1, errors.size());
+        assertEquals("User ID", errors.get(0).getField());
+        assertEquals("User with this property not found.", errors.get(0).getMessage());
+    }
+
+    @Test
+    public void shouldNotReturnErrors() {
+        FindUserBetsByUserIdRequest request = new FindUserBetsByUserIdRequest(2L);
+        Mockito.when(userRepository.existsById(2L)).thenReturn(true);
+        List<CoreError> errors = validator.validate(request);
+
+        assertEquals(0, errors.size());
+    }
+
+}
