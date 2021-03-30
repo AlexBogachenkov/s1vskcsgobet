b44f218fa85b9e2f9b24e775b8673ced975660ce
AlexBogachenkov <aleksbogac@gmail.com>
2021-03-30 20:05:28 +0300

    Added LoginUser service + controller

--- src/main/java/s1vskcsgobet/core/services/UserService.java
+++ src/main/java/s1vskcsgobet/core/services/UserService.java
@@ -1,32 +1,42 @@
 package s1vskcsgobet.core.services;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import s1vskcsgobet.core.database.UserRepository;
+import s1vskcsgobet.core.domain.Role;
 import s1vskcsgobet.core.domain.User;
-import s1vskcsgobet.core.requests.user.AddUserRequest;
-import s1vskcsgobet.core.requests.user.DeleteUserByNicknameRequest;
-import s1vskcsgobet.core.requests.user.TopUpUserBalanceRequest;
-import s1vskcsgobet.core.requests.user.WithdrawFromUserBalanceRequest;
+import s1vskcsgobet.core.requests.user.*;
 import s1vskcsgobet.core.responses.CoreError;
-import s1vskcsgobet.core.responses.user.AddUserResponse;
-import s1vskcsgobet.core.responses.user.DeleteUserByNicknameResponse;
-import s1vskcsgobet.core.responses.user.FindAllUsersResponse;
+import s1vskcsgobet.core.responses.user.*;
 import s1vskcsgobet.core.validators.user.AddUserRequestValidator;
 import s1vskcsgobet.core.validators.user.DeleteUserByNicknameRequestValidator;
+import s1vskcsgobet.core.validators.user.LoginUserRequestValidator;
+import s1vskcsgobet.core.validators.user.SignupUserRequestValidator;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
     private final AddUserRequestValidator addUserRequestValidator;
+    private final SignupUserRequestValidator signupUserRequestValidator;
+    private final LoginUserRequestValidator loginUserRequestValidator;
     private final DeleteUserByNicknameRequestValidator deleteUserByNicknameRequestValidator;
 
-    public UserService(UserRepository userRepository, AddUserRequestValidator addUserRequestValidator, DeleteUserByNicknameRequestValidator deleteUserByNicknameRequestValidator) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder encoder,
+                       AddUserRequestValidator addUserRequestValidator,
+                       SignupUserRequestValidator signupUserRequestValidator,
+                       LoginUserRequestValidator loginUserRequestValidator,
+                       DeleteUserByNicknameRequestValidator deleteUserByNicknameRequestValidator) {
         this.userRepository = userRepository;
+        this.encoder = encoder;
         this.addUserRequestValidator = addUserRequestValidator;
+        this.signupUserRequestValidator = signupUserRequestValidator;
+        this.loginUserRequestValidator = loginUserRequestValidator;
         this.deleteUserByNicknameRequestValidator = deleteUserByNicknameRequestValidator;
     }
 
@@ -38,6 +48,28 @@ public class UserService {
         User user = new User(request.getUserNickname(), request.getUserPassword(), request.getUserBalance(), request.getUserRole());
         User addedUser = userRepository.save(user);
         return new AddUserResponse(addedUser);
+    }
+
+    public SignupUserResponse signup(SignupUserRequest request) {
+        List<CoreError> errors = signupUserRequestValidator.validate(request);
+        if (!errors.isEmpty()) {
+            return new SignupUserResponse(errors);
+        }
+        User user = new User();
+        user.setNickname(request.getNickname());
+        user.setPassword(encoder.encode(request.getPassword()));
+        user.setBalance(new BigDecimal("1000.00"));
+        user.setRole(Role.USER);
+        userRepository.save(user);
+        return new SignupUserResponse(true);
+    }
+
+    public LoginUserResponse login(LoginUserRequest request) {
+        List<CoreError> errors = loginUserRequestValidator.validate(request);
+        if (!errors.isEmpty()) {
+            return new LoginUserResponse(errors);
+        }
+        return new LoginUserResponse(true);
     }
 
     @Transactional
