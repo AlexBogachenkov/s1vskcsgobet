591031c1130c2143d7936c406c907478e463f245
AlexBogachenkov <aleksbogac@gmail.com>
2021-03-30 20:06:03 +0300

    Added SignupPage controller

--- src/main/java/s1vskcsgobet/web_ui/controllers/SignupPageController.java
+++ src/main/java/s1vskcsgobet/web_ui/controllers/SignupPageController.java
@@ -1,14 +1,36 @@
 package s1vskcsgobet.web_ui.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import s1vskcsgobet.core.requests.user.SignupUserRequest;
+import s1vskcsgobet.core.responses.user.SignupUserResponse;
+import s1vskcsgobet.core.services.UserService;
 
 @Controller
 public class SignupPageController {
 
+    private UserService userService;
+
+    public SignupPageController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping(value = "/signup")
-    public String showWelcomePage() {
+    public String showSignupPage() {
         return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String processSignupRequest(@ModelAttribute(value = "request") SignupUserRequest request, ModelMap modelMap) {
+        SignupUserResponse response = userService.signup(request);
+        if (response.hasErrors()) {
+            modelMap.addAttribute("errors", response.getErrors());
+            return "signup";
+        }
+        return "bets";
     }
 
 }
