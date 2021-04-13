7a18323c59d95b77cf7a2ea547c00a0340569f12
AlexBogachenkov <aleksbogac@gmail.com>
2021-04-13 14:55:50 +0300

    Removed imports from Role

--- src/main/java/s1vskcsgobet/core/domain/Role.java
+++ src/main/java/s1vskcsgobet/core/domain/Role.java
@@ -1,10 +1,5 @@
 package s1vskcsgobet.core.domain;
 
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public enum Role {
 
     USER,
