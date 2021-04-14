6e388de6da27f0d9158537c90b9f9957f003a16b
AlexBogachenkov <aleksbogac@gmail.com>
2021-04-14 22:00:53 +0300

    Added role accesses for TeamRestController

--- src/main/java/s1vskcsgobet/web_ui/controllers/rest/TeamRestController.java
+++ src/main/java/s1vskcsgobet/web_ui/controllers/rest/TeamRestController.java
@@ -1,5 +1,6 @@
 package s1vskcsgobet.web_ui.controllers.rest;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import s1vskcsgobet.core.requests.team.AddTeamRequest;
 import s1vskcsgobet.core.requests.team.DeleteTeamByNameRequest;
@@ -10,6 +11,7 @@ import s1vskcsgobet.core.services.TeamService;
 
 @RestController
 @RequestMapping("/team")
+@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
 public class TeamRestController {
 
     private final TeamService teamService;
@@ -23,6 +25,7 @@ public class TeamRestController {
         return teamService.add(request);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping(path = "/{name}", produces = "application/json")
     public DeleteTeamByNameResponse deleteTeamByName(@PathVariable String name) {
         DeleteTeamByNameRequest request = new DeleteTeamByNameRequest();
