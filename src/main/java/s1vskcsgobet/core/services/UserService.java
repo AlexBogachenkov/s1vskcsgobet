c14f8e1400d5406f9068b5fa75a94db698a4d7a7
AlexBogachenkov <aleksbogac@gmail.com>
2021-04-15 11:00:33 +0300

    Deleted custom login implementation

--- src/main/java/s1vskcsgobet/core/services/UserService.java
+++ src/main/java/s1vskcsgobet/core/services/UserService.java
@@ -1,6 +1,5 @@
 package s1vskcsgobet.core.services;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import s1vskcsgobet.core.database.UserRepository;
@@ -8,10 +7,12 @@ import s1vskcsgobet.core.domain.Role;
 import s1vskcsgobet.core.domain.User;
 import s1vskcsgobet.core.requests.user.*;
 import s1vskcsgobet.core.responses.CoreError;
-import s1vskcsgobet.core.responses.user.*;
+import s1vskcsgobet.core.responses.user.AddUserResponse;
+import s1vskcsgobet.core.responses.user.DeleteUserByNicknameResponse;
+import s1vskcsgobet.core.responses.user.FindAllUsersResponse;
+import s1vskcsgobet.core.responses.user.SignupUserResponse;
 import s1vskcsgobet.core.validators.user.AddUserRequestValidator;
 import s1vskcsgobet.core.validators.user.DeleteUserByNicknameRequestValidator;
-import s1vskcsgobet.core.validators.user.LoginUserRequestValidator;
 import s1vskcsgobet.core.validators.user.SignupUserRequestValidator;
 
 import java.math.BigDecimal;
@@ -24,17 +25,14 @@ public class UserService {
     private final UserRepository userRepository;
     private final AddUserRequestValidator addUserRequestValidator;
     private final SignupUserRequestValidator signupUserRequestValidator;
-    private final LoginUserRequestValidator loginUserRequestValidator;
     private final DeleteUserByNicknameRequestValidator deleteUserByNicknameRequestValidator;
 
     public UserService(UserRepository userRepository, AddUserRequestValidator addUserRequestValidator,
                        SignupUserRequestValidator signupUserRequestValidator,
-                       LoginUserRequestValidator loginUserRequestValidator,
                        DeleteUserByNicknameRequestValidator deleteUserByNicknameRequestValidator) {
         this.userRepository = userRepository;
         this.addUserRequestValidator = addUserRequestValidator;
         this.signupUserRequestValidator = signupUserRequestValidator;
-        this.loginUserRequestValidator = loginUserRequestValidator;
         this.deleteUserByNicknameRequestValidator = deleteUserByNicknameRequestValidator;
     }
 
@@ -60,14 +58,6 @@ public class UserService {
         user.setRole(Role.USER);
         userRepository.save(user);
         return new SignupUserResponse(true);
-    }
-
-    public LoginUserResponse login(LoginUserRequest request) {
-        List<CoreError> errors = loginUserRequestValidator.validate(request);
-        if (!errors.isEmpty()) {
-            return new LoginUserResponse(errors);
-        }
-        return new LoginUserResponse(true);
     }
 
     @Transactional
--- src/main/java/s1vskcsgobet/web_ui/controllers/LoginPageController.java
+++ src/main/java/s1vskcsgobet/web_ui/controllers/LoginPageController.java
@@ -1,37 +1,14 @@
 package s1vskcsgobet.web_ui.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import s1vskcsgobet.core.requests.user.LoginUserRequest;
-import s1vskcsgobet.core.responses.user.LoginUserResponse;
-import s1vskcsgobet.core.services.UserService;
 
 @Controller
 public class LoginPageController {
 
-    private UserService userService;
-
-    public LoginPageController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping(value = "/login")
-    public String showLoginPage(ModelMap modelMap) {
-        modelMap.addAttribute("request", new LoginUserRequest());
+    public String showLoginPage() {
         return "login";
-    }
-
-    @PostMapping(value = "/login")
-    public String processLoginRequest(@ModelAttribute(value = "request") LoginUserRequest request, ModelMap modelMap) {
-        LoginUserResponse response = userService.login(request);
-        if (response.hasErrors()) {
-            modelMap.addAttribute("errors", response.getErrors());
-            return "login";
-        }
-        return "bets";
     }
 
 }
