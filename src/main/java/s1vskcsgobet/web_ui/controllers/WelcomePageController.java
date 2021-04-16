5fbfea214dac8000688087e3778a1fde8da16a58
AlexBogachenkov <aleksbogac@gmail.com>
2021-04-16 22:08:59 +0300

    Added automatic redirecting from "/" to "bets" if user is logged in

--- src/main/java/s1vskcsgobet/web_ui/controllers/WelcomePageController.java
+++ src/main/java/s1vskcsgobet/web_ui/controllers/WelcomePageController.java
@@ -2,13 +2,19 @@ package s1vskcsgobet.web_ui.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 @Controller
 public class WelcomePageController {
 
     @GetMapping(value = "/")
-    public String showWelcomePage() {
-        return "index";
+    public ModelAndView showWelcomePage(Principal principal) {
+        if (principal == null) {
+            return new ModelAndView("index");
+        }
+        return new ModelAndView("redirect:/bets");
     }
 
 }
